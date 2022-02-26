c47ce3c16fb38d3fdc1cd2433717662ea05bffa6
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-26 12:20:26 -0800

    BE-stash

--- backend/index.ts
+++ backend/index.ts
@@ -26,6 +26,15 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/api/coin/:ticker', async (req, res) => {
+  console.log('req backend:', req);
+  const { ticker } = req.params;
+  const data = await CoinGeckoClient.coins.fetch(ticker);
+  console.log('data:', data);
+  res.send(data);
+});
+
+app.get('/simple/price', (req, res) => {});
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
