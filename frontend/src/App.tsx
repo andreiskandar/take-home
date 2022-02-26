6aaa76ee99284d47257001baabb94a3a83f98f92
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-26 13:31:14 -0800

    start developing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { TextField, Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
+type Ticker = {
+  ticker: string;
+  value: string;
+};
+
 function App() {
+  const { handleSubmit, register } = useForm();
+
+  useEffect(() => {
+    axios.get('/').then((res) => console.log(res));
+  }, []);
+
+  const onSubmit = (data: Ticker) => {
+    const { ticker } = data;
+    // api call to the backend
+    const res = axios.get(`/api/coin/${ticker}/`).then((data) => {
+      console.log(JSON.stringify(data));
+    });
+
+    // clear input
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField id='ticker' variant='outlined' {...register('ticker')} />
+        <Button type='submit' variant='contained'>
+          Get Price
+        </Button>
+      </form>
     </div>
   );
 }
