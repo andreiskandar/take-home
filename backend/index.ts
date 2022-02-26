abbe39fd96cc93c16fced6280552b1fb3580cd9c
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-26 12:16:50 -0800

    install cors

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const pino = require('pino');
 const expressPino = require('express-pino-logger');
 
+const cors = require('cors');
 const app = express();
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
 const logger = pino({
   prettifier: require('pino-colada'),
 });
@@ -12,7 +16,8 @@ const expressLogger = expressPino({ logger });
 
 app.use(expressLogger);
 app.use(bodyParser.json());
-const PORT = 8080;
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
