4ecc158166eef86a14c74fdab8911b14fb8a8807
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-27 12:06:34 -0800

    follow interface naming convention

--- frontend/src/components/Price.tsx
+++ frontend/src/components/Price.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactElement } from 'react';
-import { PriceProps } from '../interface/Price';
+import { IPriceProps } from '../interface/IPriceProps';
 
-const Price: FC<PriceProps> = ({ currency, price }): ReactElement => {
+const Price: FC<IPriceProps> = ({ currency, price }): ReactElement => {
   return (
     <tr>
       <td>{currency}</td>
--- frontend/src/interface/IPriceProps.ts
+++ frontend/src/interface/IPriceProps.ts
@@ -1,4 +1,4 @@
-export interface PriceProps {
+export interface IPriceProps {
   currency: string;
   price: any;
 }
