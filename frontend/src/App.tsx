8cf222746024e635d4f73d5677a65464537922d0
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-27 13:16:02 -0800

    refactor code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,100 +1,34 @@
-import { useState } from 'react';
-import axios from 'axios';
-import {
-  TextField,
-  Button,
-  Container,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Table,
-  Paper
-} from '@material-ui/core';
-import { useForm } from 'react-hook-form';
+import { useState, ReactElement } from 'react';
+import { Container } from '@material-ui/core';
 import './App.css';
+
 import Price from './components/Price';
-import { IErrorProps } from './interface/IErrorProps';
 import Error from './components/Error';
+import PriceTable from './components/PriceTable';
+import InputForm from './components/InputForm';
+
+import { IErrorProps } from './interface/IErrorProps';
 
 function App() {
-  const { handleSubmit, register, resetField } = useForm();
-  const [prices, setPrices] = useState({});
+  const [pricesData, setPricesData] = useState<object>({});
   const [error, setError] = useState<IErrorProps>({
     error: false,
     message: ''
   });
 
-  const onSubmit = (data: any) => {
-    const { ticker } = data;
-    let errorMessage = '';
-    if (ticker) {
-      // api call to the backend
-      const res = axios
-        .get(`/api/coin/${ticker}/`)
-        .then(({ data }) => {
-          if (data.error) {
-            errorMessage = data.error;
-            setError({ error: true, message: errorMessage });
-            return;
-          }
-
-          const { market_data, name } = data;
-          const { current_price } = market_data;
-          setPrices(current_price);
-
-          reset();
-        })
-        .catch((e) => {
-          errorMessage = 'Something is wrong. Please contact administrator';
-          setError({ error: true, message: errorMessage });
-        });
-    } else {
-      setError({ error: true, message: 'Please enter ticker!' });
-    }
-  };
-
-  const reset = (): void => {
-    resetField('ticker');
-    setError({ error: false, message: '' });
-  };
-
-  const renderPrices = () => {
-    return Object.entries(prices).map(([currency, price]) => (
+  const renderPrices = (): ReactElement[] => {
+    return Object.entries(pricesData).map(([currency, price]) => (
       <Price key={currency} currency={currency} price={price} />
     ));
   };
 
   return (
     <Container className="App" fixed>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          id="ticker"
-          variant="outlined"
-          {...register('ticker')}
-          placeholder="Please Enter Ticker"
-        />
-        <Button type="submit" variant="contained">
-          Get Price
-        </Button>
-      </form>
+      <InputForm setPricesData={setPricesData} setError={setError} />
       {error.error ? (
         <Error error={error.error} message={error.message} />
       ) : (
-        Object.values(prices).length > 0 && (
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Currency</TableCell>
-                  <TableCell>Price</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>{renderPrices()}</TableBody>
-            </Table>
-          </TableContainer>
-        )
+        Object.values(pricesData).length > 0 && <PriceTable renderPrices={renderPrices} />
       )}
     </Container>
   );
