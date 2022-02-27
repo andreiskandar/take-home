5a6132550ba87ae66aa95b42ef3772b919819617
Andre Iskandar <andre.m.iskandar@gmail.com>
2022-02-27 13:16:27 -0800

    add styling for price row

--- frontend/src/components/Price.tsx
+++ frontend/src/components/Price.tsx
@@ -5,8 +5,8 @@ import { IPriceProps } from '../interface/IPriceProps';
 const Price: FC<IPriceProps> = ({ currency, price }): ReactElement => {
   return (
     <TableRow>
-      <TableCell>{currency}</TableCell>
-      <TableCell>{Intl.NumberFormat('en-US').format(price)}</TableCell>
+      <TableCell align="center">{currency}</TableCell>
+      <TableCell align="right">{Intl.NumberFormat('en-US').format(price)}</TableCell>
     </TableRow>
   );
 };
